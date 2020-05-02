f2933104ec9c798acf9dd2d99c758ecfd0b98258
haberman <haberman@protonmail.com>
2020-05-02 23:47:42 +0200

    a build for the lin

---
+++ build.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const Builder = std.build.Builder;
+
+pub fn build(b: *Builder) void {
+    const build_mode = b.standardReleaseOptions();
+
+    const lib = b.addStaticLibrary("strings", "src/strings.zig");
+    lib.setBuildMode(build_mode);
+    lib.linkLibC();
+    lib.install();
+
+    const test_step = b.addTest("tests/tests.zig");
+    test_step.addPackagePath("strings", "src/strings.zig");
+    test_step.setBuildMode(build_mode);
+    test_step.linkLibrary(lib);
+
+    const test_cmd = b.step("test", "Run the tests");
+    test_cmd.dependOn(&test_step.step);
+
+    const benchmark_exe = b.addExecutable("benchmarks", "tests/benchmarks.zig");
+    benchmark_exe.addPackagePath("strings", "src/strings.zig");
+    benchmark_exe.setBuildMode(build_mode);
+    benchmark_exe.linkLibrary(lib);
+    benchmark_exe.install();
+
+    const benchmark_cmd = benchmark_exe.run();
+    benchmark_cmd.step.dependOn(&benchmark_exe.step);
+
+    const benchmark_step = b.step("benchmark", "Benchmark some features");
+    benchmark_step.dependOn(&benchmark_cmd.step);
+}
